b7d3b59e945985e0ce6caa835353fb1e4e026b64
daniel <d361120041@yahoo.com.tw>
2025-06-18 10:50:20 +0800

    新增google icon

--- src/components/ActivityList.vue
+++ src/components/ActivityList.vue
@@ -9,6 +9,7 @@
                 <td>結束時間</td>
                 <td>備註</td>
                 <td>心情</td>
+                <td colspan="2"></td>
             </tr>
         </thead>
         <tbody>
@@ -20,29 +21,48 @@
                 <td>{{ activity.endTime }}</td>
                 <td>{{ activity.notes }}</td>
                 <td>{{ activity.mood }}</td>
+                <td><span class="material-symbols-outlined">edit</span></td>
+                <td><span class="material-symbols-outlined">delete</span></td>
             </tr>
         </tbody>
     </table>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-import api from '@/api/api.js'
+const props = defineProps(['activities'])
 
-const activities = ref([])
-
-async function getActivitiesByDate() {
-    try {
-        const response = await api.get('/activities/byDate?userId=5f839465-daef-49d8-a9f7-0802762d938e&activityDate=1970-01-01')
-        activities.value = response.data
-    } catch (error) {
-        console.log(`error->`, error)
-    }
-}
-
-onMounted(() => {
-    getActivitiesByDate()
-})
 </script>
 
-<style></style>
+<style scoped>
+table {
+    margin: 0 auto;
+    box-shadow: 0 0.5px 5px 1px #CCC;
+}
+
+table,
+td {
+    text-align: center;
+    border-collapse: collapse;
+    padding: 1rem;
+}
+
+thead {
+    background-color: #AACCFFAA;
+    border-bottom: 1px solid #DDD;
+}
+
+tbody>tr {
+    background-color: #FFF;
+    border-bottom: 1px solid #DDD;
+}
+
+td span:hover {
+    transform: scale(1.01) translateY(-1px);
+    cursor: pointer;
+}
+
+td span:active {
+    transform: scale(1) translateY(0);
+    cursor: pointer;    
+}
+</style>
